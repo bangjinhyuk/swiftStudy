89c5432cd7e440100e2c802591111601e4cbc6f2
bangjinhyuk <jinhyuk0103@gmail.com>
2021-03-07 23:48:47 +0900

    [ADD]chapter 4,5

---
+++ Chapter5.playground/Contents.swift
@@ -0,0 +1,72 @@
+import UIKit
+
+//: 정수형 데이터 타입 Int , UInt
+print("Int32 Min = \(Int32.min) Int32 Max = \(Int32.max)")
+
+//: 부동 소수점 데이터 타입
+var a = 3.141592
+var b = 3.0
+print(type(of: a))
+
+//문자열 보간
+var name = "bang"
+
+var message = "\(name) knows \(a)"
+
+print(message)
+
+var multiline = """
+start ios develope
+    
+"I thought it is fun"
+
+"""
+print(multiline)
+
+//:escape sequence
+var newline = "\n"
+
+var backslash = "\\"
+
+//: 변수
+var userCount = 10
+//:상수
+let maxUserCount = 20
+//:타입 선언
+var userCount_1: Int = 10
+//:상수 타입 선언후 값 할당
+let bookTitle : String
+
+if false {
+    bookTitle = "SwiftUI Essentials"
+}else {
+    bookTitle = "Android Studio Development Essentials"
+}
+print(bookTitle)
+//:옵셔널
+var index : Int?
+if index != nil{
+    print("not_nil")
+}else{
+    print("nil")
+}
+index = 3
+
+var treeArray = ["oak","pine","yew","Birch"]
+//:강제적 언래핑
+if index != nil{
+    print(treeArray[index!])
+}else{
+    print("nils")
+}
+//:옵셔널 바인딩
+if let myValue = index{
+    print(treeArray[myValue])
+}else{
+    print("nils")
+}
+
+//: 타입 캐스팅
+
+
+
---
+++ startPlayground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var str = "Hello, playground"
+print("welcome to swift")
+
+/*:
+ # welcome to Playgrounds
+ This is your *first* playground which is intended to demonstrate:
+ * The use of **Quick Look**
+ *placing results ** in-line** with the code
+ */
+var x = 10
+
+for index in 1...20{
+    let y = index * x
+    x -= 1
+    print(y)
+}
